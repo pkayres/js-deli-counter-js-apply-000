754d8d070bfcb797feadd28dc792a71721811833
Pete A <peter.ayresmusic@gmail.com>
2019-02-14 20:17:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ function takeANumber(currentLine,name){
 currentLine.push(name)
     return `Welcome, ${name}. You are number ${currentLine.length} in line.`
   }
-function nowServing(deliLine){
-  if (deliLine.length == 0){
+
+function nowServing(currentLine){
+  if (currentLine.length == 0){
     return 'There is nobody waiting to be served!'
   } else {
-    for (let i = 0; i < deliLine.length; i++){
-      var deliLineGuy = deliLine[i];
-      deliLine.shift(deliLineGuy)
+      var deliLineGuy = currentLine[0];
+      currentLine.shift(deliLineGuy)
          return `Currently serving ${deliLineGuy}.`
 
     }
   }
-}
+
 function currentLine(line){
   if (line.length == 0){
     return 'The line is currently empty.'
